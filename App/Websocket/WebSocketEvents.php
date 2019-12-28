e52d8ebfa8f332529ee66c689d337bb112c093b0
shenhengxin <shenhengxin@qq.com>
2019-12-28 15:38:48 +0800

    基础框架

--- App/Websocket/WebSocketEvents.php
+++ App/Websocket/WebSocketEvents.php
@@ -8,11 +8,11 @@ class WebSocketEvents
 {
     public static function onOpen(\swoole_websocket_server $server, \swoole_http_request $request)
     {
-        $server->push($request->fd, "客户端:".$request->fd."连接上了\n");
+        //$server->push($request->fd, "客户端:".$request->fd."连接上了\n");
     }
 
     public static function onClose(\swoole_server $server, int $fd, int $reactorId)
     {
-        $server->push($fd->fd, "客户端:".$fd->fd."断开连接了\n");
+        //$server->push($fd->fd, "客户端:".$fd->fd."断开连接了\n");
     }
 }
---
+++ Storage/framework/cache/views/1a7a05ac7635dfdae0be9ef7275cd4c577ae9bf7.php
@@ -0,0 +1,2 @@
+<?php
+echo asset("css/admin.css"); ?><?php /**PATH /home/vagrant/code/easyswoole/Resources/views/index.blade.php ENDPATH**/ ?>
